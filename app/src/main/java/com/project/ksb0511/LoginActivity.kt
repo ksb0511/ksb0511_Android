26128dc467448bbb8ce755a8d2fe928bad7925e1
ksb0511 <soobin796@gmail.com>
2020-01-26 02:34:17 +0900

    login move main

--- app/src/main/java/com/project/ksb0511/LoginActivity.kt
+++ app/src/main/java/com/project/ksb0511/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.project.ksb0511
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+    }
+
+    private fun login(){
+
+        login.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
